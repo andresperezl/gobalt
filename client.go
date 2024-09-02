b4660d53ca5c51d66fb8175897bd7f05c3bd7288
Andres Perez <1676612+andresperezl@users.noreply.github.com>
2024-09-01 22:29:44 -0400

    fix: parse media filename correctly

--- client.go
+++ client.go
@@ -71,42 +71,57 @@ func (c *Cobalt) Get(ctx context.Context, params Request) (*Media, error) {
 		return nil, err
 	}
 
-	if err := media.parseFilename(resp); err != nil {
-		return nil, fmt.Errorf("error extracting media filename: %w", err)
-	}
-
 	return media, nil
 }
 
-func (m *Media) parseFilename(resp *http.Response) error {
+// ParseFilename will try to extract the filename depending on the type of the m.StatusResponse.
+// This is intended to be used with the *http.Response when calling the URL pointedb by m.URL
+//
+// When m.StatusResponse == StatusResponseRedirect, the filename will be set based on the basename of the URL path
+//
+// When m.StatusResponse == StatusResponseStream, the filename will be extracted from the Content-Disposition header
+//
+// All other unsupported methods leave the m.Filename empty
+// Errors returned are unexpected, and will be a consenquence of a parsing error.
+func (m *Media) ParseFilename(resp *http.Response) error {
 	if m.Status == ResponseStatusError || m.Status == ResponseStatusRateLimit {
 		return nil
 	}
 
-	for _, cd := range resp.Header.Values("Content-Disposition") {
-		_, params, err := mime.ParseMediaType(cd)
+	if m.Status == ResponseStatusRedirect {
+		parsedURL, err := url.Parse(m.URL)
 		if err != nil {
 			return err
 		}
-		if filename, ok := params["filename"]; ok {
-			m.filename = filename
-			return nil
+		m.filename = path.Base(parsedURL.Path)
+		return nil
+	}
+
+	if m.Status == ResponseStatusStream {
+		cd := resp.Header.Get("Content-Disposition")
+		if cd != "" {
+			_, params, err := mime.ParseMediaType(cd)
+			if err != nil {
+				return err
+			}
+			if filename, ok := params["filename"]; ok {
+				m.filename = filename
+			}
 		}
 	}
-	parsedURL, err := url.Parse(m.URL)
-	if err != nil {
-		return err
-	}
-	m.filename = path.Base(parsedURL.Path)
+
 	return nil
 }
 
+// Filename will return the filename associated with this media. ParseFilename must be called first, either directly or indirectly via m.Stream().
+// Not doing so will keep the filename empty.
 func (m *Media) Filename() string {
 	return m.filename
 }
 
 // Stream is a helper utility that will return an io.ReadCloser using the URL from this media object
 // The returned io.ReadCloser is the Body of *http.Response and must be closed when you are done with the stream.
+// Stream will also call ParseFilename, so m.Filename() will be set
 func (m *Media) Stream(ctx context.Context) (io.ReadCloser, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, m.URL, nil)
 	if err != nil {
@@ -115,6 +130,10 @@ func (m *Media) Stream(ctx context.Context) (io.ReadCloser, error) {
 
 	resp, err := m.client.Do(req)
 	if err != nil {
+		return nil, err
+	}
+	if err := m.ParseFilename(resp); err != nil {
+		defer resp.Body.Close()
 		return nil, err
 	}
 
